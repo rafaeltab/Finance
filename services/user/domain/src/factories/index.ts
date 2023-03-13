0b30c2b0716b266d34f1300e1fed0cacfd9bd36d
Rafael <ikbenrafaelbieze2001@gmail.com>
2023-03-13 20:39:50 +0100

    Add lint fix

--- services/user/domain/src/factories/index.ts
+++ services/user/domain/src/factories/index.ts
@@ -1,7 +1,6 @@
 export * from "./AssetFactory";
 export * from "./AssetGroupFactory";
 export * from "./BankAccountFactory";
-export * from "./index";
 export * from "./JobFactory";
 export * from "./UserFactory";
 export * from "./StockFactory";
